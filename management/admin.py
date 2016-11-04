f0563ed2c691b980e28b6779582cdf2580b54ada
Subv <subv2112@gmail.com>
2016-11-04 17:51:11 +0000

    Show the latest route pings in the map as markers

--- management/admin.py
+++ management/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from models import BusLocationPings
+from models import BusLocationPings, BusRoute, RouteEndpoint
+
 
 @admin.register(BusLocationPings)
 class BusLocationPingsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BusRoute)
+class BusRouteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RouteEndpoint)
+class RouteEndpointAdmin(admin.ModelAdmin):
     pass
--- management/models.py
+++ management/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+from django.core import urlresolvers
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.contrib.auth.models import User
 from geoposition.fields import GeopositionField
@@ -8,6 +10,9 @@ from geoposition.fields import GeopositionField
 
 class Company(models.Model):
     name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
     
 
 class Client(User):
@@ -32,14 +37,32 @@ class Driver(models.Model):
     id_number = models.CharField(max_length=50)
     age = models.IntegerField()
     contract_number = models.OneToOneField(to=DriverContract, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return self.name
+
+
+class RouteEndpoint(models.Model):
+    name = models.CharField(max_length=255)
+    position = GeopositionField()
+    
+    def __str__(self):
+        return self.name
 
 
 class BusRoute(models.Model):
     bus = models.ForeignKey(to=Bus)
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField()
-    origin = models.CharField(max_length=255)
-    destiny = models.CharField(max_length=255)
+    origin = models.ForeignKey(to=RouteEndpoint, related_name="as_origin")
+    destination = models.ForeignKey(to=RouteEndpoint, related_name="as_destination")
+    
+    def get_admin_url(self):
+        content_type = ContentType.objects.get_for_model(self.__class__)
+        return urlresolvers.reverse("admin:%s_%s_change" % (content_type.app_label, content_type.model), args=(self.id,))
+        
+    def __str__(self):
+        return "%s - %s (%s)" % (self.origin, self.destination, self.departure_time)
 
     
 class BusLocationPings(models.Model):
